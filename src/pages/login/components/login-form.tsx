01cd17d0d57eb23c42eb2d164d54ca095ab502fc
tonynguyen <tonyjsfreelancer@gmail.com>
2025-01-06 19:51:24 +0700

    build login form

--- src/pages/login/components/login-form.tsx
+++ src/pages/login/components/login-form.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '@refinedev/react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 // material-ui
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
 import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import IconButton from '@mui/material/IconButton';
 import InputAdornment from '@mui/material/InputAdornment';
 import InputLabel from '@mui/material/InputLabel';
@@ -18,9 +19,31 @@ import Typography from '@mui/material/Typography';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { PATH_NAME } from '@/configs/path-name';
+import FormHelperText from '@mui/material/FormHelperText/FormHelperText';
+
+const validationSchema: any = yup.object().shape({
+  email: yup.string().email('Invalid email address').required('Email is required'),
+  password: yup.string().required('Password is required'),
+  confirm_password: yup
+    .string()
+    .required('Confirm Password is required')
+    .oneOf([yup.ref('password'), ''], 'Passwords must match'),
+});
 
 const LoginForm = () => {
   const theme = useTheme();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, touchedFields },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      confirm_password: '',
+    },
+  });
 
   const [showPassword, setShowPassword] = React.useState(false);
   const handleClickShowPassword = () => {
@@ -31,25 +54,37 @@ const LoginForm = () => {
     event.preventDefault();
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const onSubmit = (data: any) => {
+    console.log('Form Data:', data);
     window.localStorage.setItem('access_token', 'dasdasdsa');
     window.location.href = PATH_NAME.ROOT;
   };
 
   return (
-    <form noValidate onSubmit={handleSubmit}>
-      <FormControl fullWidth sx={{ ...theme.typography.customInput }}>
+    <form noValidate onSubmit={handleSubmit(onSubmit)}>
+      <FormControl fullWidth sx={{ ...theme.typography.customInput }} error={Boolean(touchedFields.email && errors.email)}>
         <InputLabel htmlFor="outlined-adornment-email-login">Email Address / Username</InputLabel>
-        <OutlinedInput id="outlined-adornment-email-login" type="email" name="email" inputProps={{}} />
+        <OutlinedInput
+          id="outlined-adornment-email-login"
+          type="email"
+          inputProps={{}}
+          {...register('email')}
+          error={!!errors.email}
+        />
+        {touchedFields.email && errors.email && (
+          <FormHelperText error id="standard-weight-helper-text-email-login">
+            {errors.email.message}
+          </FormHelperText>
+        )}
       </FormControl>
 
-      <FormControl fullWidth sx={{ ...theme.typography.customInput }}>
+      <FormControl fullWidth sx={{ ...theme.typography.customInput }} error={Boolean(touchedFields.password && errors.password)}>
         <InputLabel htmlFor="outlined-adornment-password-login">Password</InputLabel>
         <OutlinedInput
           id="outlined-adornment-password-login"
           type={showPassword ? 'text' : 'password'}
-          name="password"
+          {...register('password')}
+          error={!!errors.password}
           endAdornment={
             <InputAdornment position="end">
               <IconButton
@@ -66,6 +101,45 @@ const LoginForm = () => {
           inputProps={{}}
           label="Password"
         />
+        {touchedFields.password && errors.password && (
+          <FormHelperText error id="standard-weight-helper-text-email-login">
+            {errors.password.message}
+          </FormHelperText>
+        )}
+      </FormControl>
+
+      <FormControl
+        fullWidth
+        sx={{ ...theme.typography.customInput }}
+        error={Boolean(touchedFields.confirm_password && errors.confirm_password)}
+      >
+        <InputLabel htmlFor="outlined-adornment-password-login">Confirm Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password-login"
+          type={showPassword ? 'text' : 'password'}
+          {...register('confirm_password')}
+          error={!!errors.confirm_password}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+                size="large"
+              >
+                {showPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          }
+          inputProps={{}}
+          label="Password"
+        />
+        {touchedFields.confirm_password && errors.confirm_password && (
+          <FormHelperText error id="standard-weight-helper-text-email-login">
+            {errors.confirm_password.message}
+          </FormHelperText>
+        )}
       </FormControl>
 
       <Box sx={{ textAlign: 'right' }}>
